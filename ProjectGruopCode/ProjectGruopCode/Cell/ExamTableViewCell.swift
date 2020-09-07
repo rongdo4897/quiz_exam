0d02f7d7bc187a6cdae7694398c51f46dba8c36b
Hoang Lam <hoanglam@MacBook-Pro-cua-Hoang.local>
2020-09-07 07:25:12 +0700

    quiz_exam

---
+++ ProjectGruopCode/ProjectGruopCode/Cell/ExamTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ExamTableViewCell.swift
+//  ProjectGruopCode
+//
+//  Created by Chu Thanh Dat on 9/3/20.
+//  Copyright © 2020 Chu Thanh Dat. All rights reserved.
+//
+
+import UIKit
+
+class ExamTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    @IBOutlet weak var txtAnswer: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    
+    
+}
